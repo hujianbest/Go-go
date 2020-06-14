1852d73266fe7688dc18bad5129f45eb18d4a9d3
hujianbest <jianbesthu@163.com>
2020-06-14 17:54:39 +0800

    [hj] helloworld.go

---
+++ echo.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	//for i := 1; i < len(os.Args); i++ {
+	//	s += sep + os.Args[i]
+	//	sep = " "
+	//}
+
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
